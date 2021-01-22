7617fc3cb319e61c197c886802d033b6a0f40fc4
seowteng00 <ng_seow_teng@u.nus.edu>
2021-01-23 00:28:06 +0800

    updated mod1

--- mod1_solution/assignment1-starter-code/app.js
+++ mod1_solution/assignment1-starter-code/app.js
@@ -8,22 +8,21 @@
 	function LunchCheckController($scope) {
 		$scope.items = "";
 		$scope.message = "";
-		$scope.color = "";
+		$scope.color = {};
 		
 		$scope.checkLunch = function () {
 			var items = $scope.items.split(",");
 
 			if (items.length > 3) {
 				$scope.message = "Too much!";
-				$scope.color = "{ 'color': 'green' }";
-				console.log($scope.color);
+				$scope.color = { "color": "green" };
 
 			} else if (items[0] === "" && items.length === 1) {
 				$scope.message = "Please enter data first";
-				$scope.color = "{ 'color': 'red' }";
+				$scope.color = { "color": "red" };
 			} else {
 				$scope.message = "Enjoy!";
-				$scope.color = "{ 'color': 'green' }";
+				$scope.color = { "color": "green" };
 			}
 		}
 	}
